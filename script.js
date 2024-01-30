1bfa05243e325361eac3998f021dbf631455b3a3
satoru <satoru.main0301create@gmail.com>
2024-01-30 22:29:12 +0900

    “コミットテスト6”

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const symbol = require('/node_modules/symbol-sdk')
 
 const address = symbol.Address.createFromRawAddress("TBBBE65OSR32BHEO2O3NG2JRSJRPSLOQMBDV2DY")
-console.log("Hello Symbol")
-console.log(`Your Address : ${address.plain()}`)
+const dom_addr = document.getElementById('wallet-addr')
+dom_addr.innerText = address.pretty()
