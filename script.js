0473ff358f94cd404ebc162adcdd99534fae32c3
satoru <satoru.main0301create@gmail.com>
2024-01-30 22:09:57 +0900

    “コミットテスト4”

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const symbol = require('/node_modules/symbol-sdk')
 
+const address = symbol.Address.createFromRawAddress("TAD7Q3FEN5CZRZFE3WX6TWEESATVTMJDS2ETVTY")
 console.log("Hello Symbol")
+console.log(`Your Address : ${address.plain()}`)
